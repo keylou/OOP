a1d86d1d314379a51a1eb3822f4b71258544f922
keylou <116100969+keylou@users.noreply.github.com>
2023-06-03 09:34:16 +0300

    Update Source.cpp

--- OOP1/Source.cpp
+++ OOP1/Source.cpp
@@ -1,1 +1,15 @@
+#include "Triangle.h"
+#include <iostream>
+#include <Windows.h>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	Triangle triangle(3, 3, 3, 1);
+	double h1, h2, h3;
+	triangle.CalcHeights(h1, h2, h3);
+	std::cout << triangle.toString() << triangle.TriType() << triangle.CalcArea() << "\n" << triangle.CalcPerim();
+}
